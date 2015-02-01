df29117dfffbbd0bbb056180f149f3b7c0bcdf4c
xinhaowu <bio.gavin@gmail.com>
2015-02-01 11:44:16 -0800

    hw1 sub

--- assignments/hw1/hw1.R
+++ assignments/hw1/hw1.R
@@ -93,7 +93,7 @@ family.young = family[family$age<40,]
 
 # Create a new data frame 
 # << family.30y68i >> : a data frame, subset of family, with only people *over* 30, *shorter* than 68 in
-family.30y68i = family[family$age %in% c(30:60),]
+family.30y68i = family[family$age>30 & family$height<68,]
 
 # Formula for BMI : BMI = (weight in lbs) / (height in in)^2 * 703
 # Note: the dataframe has weight in lbs and height in in as required.
