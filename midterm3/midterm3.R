954af6c2ecaf1e5b7a9c2e5ce6e5461acd276f1e
xinhaowu <bio.gavin@gmail.com>
2015-04-17 13:58:53 -0700

    midterm3 sub

--- midterm3/midterm3.R
+++ midterm3/midterm3.R
@@ -9,7 +9,13 @@
 #   <num.brac>: an integer indicating how many elements of <chvec> contain the "["
 #     symbol. For example: numBracElements(c('digit', '[:digit:]', '[]')) should return 2
 
-
+numBracElements <- function(chvec) {
+        
+        char.chvec <- unlist(strsplit(chvec,""))
+        num.brac <- length(which(char.chvec =="["))
+        
+        return(num.brac)
+}
 
 
 # Write a function called maxDigits that return the maximum of all (single) digits in
@@ -20,7 +26,14 @@
 # and return the following
 #   <total>: A single number (the maximum of all digits in chvec)
 
-
+maxDigits <- function(chvec){
+        
+        digit.chvec <- substring(chvec,gregexpr("[[:digit:]]",chvec)[[1]],gregexpr("[[:digit:]]",chvec)[[1]])
+        digit.chvec <- as.numeric(digit.chvec)
+        ifelse(is.na(digit.chvec),digit.chvec <- 1)
+        total<- max(digit.chvec)
+        return(total)
+}
 
 # Some test cases:
 all.equal(maxDigits("1z3p ! 28"), 8)
@@ -30,14 +43,24 @@ all.equal(maxDigits("abcdefg"), 0)
 
 # Write a function called hisToHer that converts every instance of 
 # him in a string to her; every instance of he to she and every instance 
-# of his to hers. You can assume everything is lower case. Be careful not 
+# of his to her. You can assume everything is lower case. Be careful not 
 # to replace words that contain him/he/his (eg you don't want to
 # replace the with ther). Your function should take the argument
 #   <chvec>: A character vector
 #
 # and return
 #   <herchvec>: The same character vector with the required substitutions.
+hisToHer <- function(chvec){
+        
+        chvec <- unlist(strsplit(chvec," "))
+        chvec <- gsub("\\<(his)\\>","her",chvec)
+        herchvec <- gsub("\\<he\\>","she",chvec)
+        herchvec <- gsub("\\<him\\>","her",herchvec)
+        herchvec <- paste(herchvec,  collapse = " ")
 
+        
+        return(herchvec)
+}
 
 # A test case
 all.equal(
@@ -58,4 +81,21 @@ all.equal(
 #  <letter> The most common letter or letters in the string.
 # For example mostCommonLetter("aabbccccdddd") should return 
 # [1] "c" "d"
+
+mostCommonLetter <- function(chvec){
+        
+        chvec <- tolower(chvec)
+        chvec <- unlist(strsplit(chvec,""))
+        chvec <- chvec[grep("[a-z]",chvec)]
+        count.chvec <- table(chvec)
+        
+        letter <- names(count.chvec[count.chvec ==max(count.chvec)])
+        
+        return(letter)
+}
+
+
+
+
+
 
